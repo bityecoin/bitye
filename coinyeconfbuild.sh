65161f6545412e9300535db6bad57f5c43d4cfa1
bityecoin <coinye@gayfishpool.com>
2021-05-18 23:10:54 -0600

    more tweaks

--- coinyeconfbuild.sh
+++ coinyeconfbuild.sh
@@ -1,7 +1,9 @@
 $loc = $(pwd)
 mkdir /home/$(whoami)/.coinyecoin/
 cd /home/$(whoami)/.coinyecoin/
-> coinyecoin.conf
+rm coinyecoin.conf
+touch coinyecoin.conf
+chmod 775 coinyecoin.conf
 echo "server=1" >> coinyecoin.conf
 echo "daemon=1" >> coinyecoin.conf
 echo "listen=1" >> coinyecoin.conf
@@ -10,6 +12,6 @@ echo "rpcallow=127.0.0.1" >> coinyecoin.conf
 echo "rpcallowip=127.0.0.1" >> coinyecoin.conf
 echo "rpcconnect=127.0.0.1" >> coinyecoin.conf
 echo "rpcuser=coinye" >> coinyecoin.conf
-echo "rpcpass=coinye" >> coinyecoin.conf
+echo "rpcpassword="$(openssl rand -base64 32) >> coinyecoin.conf
 curl http://coinye.planetburt.com/schmapi/nodes.php >> coinyecoin.conf
 cd $loc
