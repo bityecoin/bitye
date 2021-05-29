b27f82cd546e436629aae09b381a4ab8ca638a4c
bityecoin <coinye@gayfishpool.com>
2021-05-28 20:29:07 -0600

    Revert "more tweaks"
    This reverts commit 65161f6545412e9300535db6bad57f5c43d4cfa1.

--- coinyeconfbuild.sh
+++ coinyeconfbuild.sh
@@ -1,9 +1,7 @@
 $loc = $(pwd)
 mkdir /home/$(whoami)/.coinyecoin/
 cd /home/$(whoami)/.coinyecoin/
-rm coinyecoin.conf
-touch coinyecoin.conf
-chmod 775 coinyecoin.conf
+> coinyecoin.conf
 echo "server=1" >> coinyecoin.conf
 echo "daemon=1" >> coinyecoin.conf
 echo "listen=1" >> coinyecoin.conf
@@ -12,6 +10,6 @@ echo "rpcallow=127.0.0.1" >> coinyecoin.conf
 echo "rpcallowip=127.0.0.1" >> coinyecoin.conf
 echo "rpcconnect=127.0.0.1" >> coinyecoin.conf
 echo "rpcuser=coinye" >> coinyecoin.conf
-echo "rpcpassword="$(openssl rand -base64 32) >> coinyecoin.conf
+echo "rpcpass=coinye" >> coinyecoin.conf
 curl http://coinye.planetburt.com/schmapi/nodes.php >> coinyecoin.conf
 cd $loc
